0eb3f8e95db3968f4c17cc4bac9ec3c70efc29ef
Denis <den.stakhovskiy.99@gmail.com>
2023-02-19 09:16:24 +0300

    adding a task

--- less4.py
+++ less4.py
@@ -1,30 +1,49 @@
 # display N numbers that are multiples of M and greater than K
 
-# num = [1, 4, 28, 36, 35, 12, 29, 9]
-# number_m = int(input("Input M: "))
-# number_k = int(input("Input K: "))
-# for i in num:
-#     if i % number_m == 0 and i > number_k:
-#         print(i)
+# N = int(input("N: "))
+# M = int(input("M: "))
+# K = int(input("K: "))
+# numbers = []
+# while len(numbers) < N:
+#     if K % M == 0:
+#         numbers.append(K)
+#         K += M
+#     else:
+#         K += 1
+# print(numbers)
 
 
 
 # create calculator
 
-i = True
-while i == True:
-    num1 = float(input("Input first number: "))
-    oper = str(input("Input operation: "))
-    num2 = float(input("Input second number: "))
-    if oper == "+":
-        total = num1 + num2
-    elif oper == "-":
-        total = num1 - num2
-    elif oper == "*":
-        total = num1 * num2
-    elif oper == "/":
-        total = num1 / num2
-    print(f"Total: {total}")
-    cont = input("Do you want continue? y/n: ")
-    if cont == "n":
-        i = False
+# i = True
+# while i == True:
+#     num1 = float(input("Input first number: "))
+#     oper = str(input("Input operation: "))
+#     num2 = float(input("Input second number: "))
+#     if oper == "+":
+#         total = num1 + num2
+#     elif oper == "-":
+#         total = num1 - num2
+#     elif oper == "*":
+#         total = num1 * num2
+#     elif oper == "/":
+#         total = num1 / num2
+#     print(f"Total: {total}")
+#     cont = input("Do you want continue? y/n: ")
+#     if cont == "n":
+#         i = False
+
+
+
+# Output even numbers from 2 to N by 5 in a line
+
+N = int(input("N: "))
+count = 0
+for i in range(2, N+1, 2):
+    if count < 5:
+        count += 1
+    else:
+        print()
+        count = 1
+    print(i, end=" ")
